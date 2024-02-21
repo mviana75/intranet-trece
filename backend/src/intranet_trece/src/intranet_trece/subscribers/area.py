dec3181ba31409a9d381130718f8f0e85d54fe6c
Marcos Viana <mviana75@gmail.com>
2024-02-21 16:57:10 -0300

    Exercício 30: criação de grupo

--- backend/src/intranet_trece/src/intranet_trece/subscribers/area.py
+++ backend/src/intranet_trece/src/intranet_trece/subscribers/area.py
@@ -2,6 +2,7 @@ from intranet_trece import logger
 from intranet_trece.content.area import Area
 from zope.lifecycleevent import ObjectAddedEvent
 from zope.lifecycleevent import ObjectModifiedEvent
+from plone import api
 
 
 def _update_excluded_from_nav(obj: Area):
@@ -10,10 +11,26 @@ def _update_excluded_from_nav(obj: Area):
     obj.exclude_from_nav = False if description else True
     logger.info(f"Atualizado o campo excluded_from_nav para {obj.title}")
 
+def _create_user_groups(obj: Area):
+    """Create user group for area"""
+    uuid = api.content.get_uuid(obj)
+    groupname = f"{uuid}_editors"
+    title = obj.title
+    payload = {
+        "groupname": groupname,
+        "title": f"Editores para {title}",
+        "description": f"Editores para a área {title} ",
+    }
+    editors = api.group.create(**payload)
+    api.group.grant_roles(group=editors, roles=["Editor"], obj=obj)
+    logger.info(
+        f"Granted role of Editor to {uuid}_editors group on {obj.absolute_url()}"
+    )    
 
 def added(obj: Area, event: ObjectAddedEvent):
     """Post creation handler for Area."""
     _update_excluded_from_nav(obj)
+    _create_user_groups(obj)
 
 def modified(obj: Area, event: ObjectModifiedEvent):
     """Post modified handler for Area."""
